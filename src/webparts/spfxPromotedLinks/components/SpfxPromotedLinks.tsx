e240c4ed555940fa3df83ed7fe7d0713b534af0e
Andrew Connell <me@andrewconnell.com>
2019-08-28 12:23:30 -0400

    updated from office hours

--- src/webparts/spfxPromotedLinks/components/SpfxPromotedLinks.tsx
+++ src/webparts/spfxPromotedLinks/components/SpfxPromotedLinks.tsx
@@ -8,22 +8,22 @@ import PromotedLinkItem from './SpfxPromotedLinkItem';
 export default class SpfxPromotedLinks extends React.Component<ISpfxPromotedLinksProps, {}> {
   public render(): React.ReactElement<ISpfxPromotedLinksProps> {
     return (
-      <div className={ styles.spfxPromotedLinks }>
-        <div className={ styles.container }>
-        <PromotedLinkItem
-            imageUrl={`https://mcclickster.sharepoint.com/sites/Templates/SPFx/Shared%20Documents/111.jpg`}
+      <div className={styles.spfxPromotedLinks}>
+        <div className={styles.container}>
+          <PromotedLinkItem
+            imageUrl={`https://www.andrewconnell.com/assets/events/201912-espc-spfx-workshop.jpg#align-right-thumb350`}
             title={`The Beach`}
             description={`Nice place to visit`}
             href={`http:www.google.com`} />
 
           <PromotedLinkItem
-            imageUrl={`https://mcclickster.sharepoint.com/sites/Templates/SPFx/Shared%20Documents/222.jpg`}
+            imageUrl={`https://www.andrewconnell.com/assets/events/201912-espc-spfx-ninja.jpg#align-right-thumb350`}
             title={`The Beach2`}
             description={`Nice place to visit`}
             href={`http:www.google.com`} />
 
           <PromotedLinkItem
-            imageUrl={`https://mcclickster.sharepoint.com/sites/Templates/SPFx/Shared%20Documents/333.jpg`}
+            imageUrl={`https://www.andrewconnell.com/assets/events/201912-espc-spfx-appinsights.jpg#align-right-thumb350`}
             title={`The Beach3`}
             description={`Nice place to visit`}
             href={`http:www.google.com`} />
