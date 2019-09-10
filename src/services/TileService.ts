7e406db103309fd16be453dbcd2bc2b51f51a38a
MikeZimm <mike.zimmerman@autoliv.com>
2019-09-10 00:51:19 -0400

    Created TileService, creating allTiles on render and setting state.
    Error is "Cannot read property "Map" of undefined....
    this.props.allTiles.map(newTile => ( .... line 127 of SpfxPromotedLinks.tsx

---
+++ src/services/TileService.ts
@@ -0,0 +1,69 @@
+
+import { IPromotedLinkItemProps } from '../webparts/spfxPromotedLinks/components/SpfxPromotedLinkItem';
+
+// randomItem = samplePics[Math.floor(Math.random()*samplePics.length)];
+export const samplePics = [
+    "https://amp.businessinsider.com/images/5698198de6183e50008b98a1-1334-1001.jpg",
+    "https://www.charlottefive.com/wp-content/uploads/2018/10/Signature-Shot-Pavilion-Pool-and-Beach.jpg",
+    "https://www.xtns.org/wp-content/uploads/2014/06/all-inclusive-resorts-island-resort-exquisite-us-virgin-islands-all-inclusive-resorts-deals-us-virgin-islands-resorts-st-croix-us-virgin-islands-resorts-st-johns-us-virgin-islands-resorts-st-thom.jpg",
+    "https://i.kinja-img.com/gawker-media/image/upload/qjmdguwovtcsjszqqgqt.jpg",
+    "https://i.pinimg.com/474x/d1/93/2a/d1932afb0987d71a61b2bf7df8735cd4.jpg",
+    "https://i.pinimg.com/474x/95/1c/c3/951cc398063945df92d7d0258e3db5a3.jpg"
+  ]
+  // randomItem = sampleCats[Math.floor(Math.random()*sampleCats.length)];
+  export const sampleCats = [
+    "People",
+    "Places",
+    "Things"
+  ]
+  // randomItem = sampleLinks[Math.floor(Math.random()*sampleLinks.length)];
+  export const sampleLinks = [
+    "https://www.google.com",
+    "https://www.bing.com",
+    "https://www.microsoft.com",
+    "https://www.cnn.com",
+    "https://www.amazon.com"
+  ]
+  
+  // https://css-tricks.com/snippets/javascript/select-random-item-array/
+  // randomItem = myArray[Math.floor(Math.random()*myArray.length)];
+  
+  export const pivtTitles = [
+    "People",
+    "Places",
+    "Things"
+  ];
+
+  export class TileService {
+  
+    public static getAllTiles(): IPromotedLinkItemProps[] {
+
+        let allTiles = [];
+
+
+        let newTile = {} as IPromotedLinkItemProps;
+
+        newTile.category = pivtTitles[Math.floor(Math.random()*pivtTitles.length)];
+        newTile.imageUrl = samplePics[Math.floor(Math.random()*samplePics.length)];
+        newTile.title = newTile.category + " " + Math.floor(Math.random() * 100);
+        newTile.description = "Description " + Math.floor(Math.random() * 10000) + " --- " + newTile.category;
+        newTile.href = sampleLinks[Math.floor(Math.random()*sampleLinks.length)];
+
+        allTiles = allTiles.concat(newTile);
+
+        
+        return allTiles;
+        
+        /*
+
+        // Why does this NOT work instead???      return <PromotedLinkItem newTile />
+
+        return <PromotedLinkItem
+        imageUrl={newTile.imageUrl}
+        title={newTile.title}
+        description={newTile.description}
+        href={newTile.href}
+        category={newTile.category} />
+        */
+    }
+  }
--- src/webparts/spfxPromotedLinks/components/SpfxPromotedLinks.tsx
+++ src/webparts/spfxPromotedLinks/components/SpfxPromotedLinks.tsx
@@ -2,6 +2,8 @@ import * as React from 'react';
 import styles from './SpfxPromotedLinks.module.scss';
 import { ISpfxPromotedLinksProps } from './ISpfxPromotedLinksProps';
 import { ISpfxPromotedLinksState } from './ISpfxPromotedLinksState';
+import { IPromotedLinkItemProps } from './SpfxPromotedLinkItem';
+
 import { escape } from '@microsoft/sp-lodash-subset';
 
 import { Pivot, PivotItem, PivotLinkSize, PivotLinkFormat } from 'office-ui-fabric-react/lib/Pivot';
@@ -11,6 +13,8 @@ import { Label } from 'office-ui-fabric-react/lib/Label';
 import PromotedLinkItem from './SpfxPromotedLinkItem';
 
 import * as tileItems from './SampleData';
+
+import { TileService } from '../../../services';
 
 export default class SpfxPromotedLinks extends React.Component<ISpfxPromotedLinksProps, ISpfxPromotedLinksState> {
 
@@ -22,29 +26,70 @@ export default class SpfxPromotedLinks extends React.Component<ISpfxPromotedLink
     }
   }
 
-  public createPivotItem(){
-    var thisPiv = tileItems.pivtTitles[Math.floor(Math.random()*tileItems.pivtTitles.length)];
+  public componentDidMount(): void {
+    this.setState({
+      allTiles: TileService.getAllTiles()
+    });
+    console.log(this.state.allTiles);
+  }
+
+  public createPivotItem(newTile){
+
+
+
+    /*
+    let newTile = {} as IPromotedLinkItemProps;
+    
+
+    newTile.category = tileItems.pivtTitles[Math.floor(Math.random()*tileItems.pivtTitles.length)];
+    newTile.imageUrl = tileItems.samplePics[Math.floor(Math.random()*tileItems.samplePics.length)];
+    newTile.title = newTile.category + " " + Math.floor(Math.random() * 100);
+    newTile.description = "Description " + Math.floor(Math.random() * 10000) + " --- " + newTile.category;
+    newTile.href = tileItems.sampleLinks[Math.floor(Math.random()*tileItems.sampleLinks.length)];
+    */
+    // concatenating state object arrays:  https://www.robinwieruch.de/react-state-array-add-update-remove
+        /*
+    this.setState(state => {
+      const allTiles = state.allTiles.concat(newTile);
+      return {
+        allTiles
+      };
+    });
+
+
+    */
+    // Why does this NOT work instead???      return <PromotedLinkItem newTile />
+
     return <PromotedLinkItem
-    imageUrl={tileItems.samplePics[Math.floor(Math.random()*tileItems.samplePics.length)]}
-    title={thisPiv + " " + Math.floor(Math.random() * 100)}
-    description={"Description " + Math.floor(Math.random() * 10000) + " --- " + thisPiv}
-    href={tileItems.sampleLinks[Math.floor(Math.random()*tileItems.sampleLinks.length)]}
-    category={thisPiv} />
+    imageUrl={newTile.imageUrl}
+    title={newTile.title}
+    description={newTile.description}
+    href={newTile.href}
+    category={newTile.category} />
 
   }
 
-    //http://react.tips/how-to-create-reactjs-components-dynamically/ - based on createImage
-    public createPivot(pivT) {
-      return (
-        <PivotItem headerText={pivT} />
-      )
-    }
+  //http://react.tips/how-to-create-reactjs-components-dynamically/ - based on createImage
+  public createPivot(pivT) {
+    return (
+      <PivotItem headerText={pivT} />
+    )
+  }
 
   public createPivots(pivtTitles){
     return (
       pivtTitles.map(this.createPivot)
     )
   }
+
+/*
+  public componentDidMount(): void {
+    this.setState({
+      tiles: MissionService.getMissions()
+    });
+  }
+*/
+
 
   public render(): React.ReactElement<ISpfxPromotedLinksProps> {
   
@@ -58,31 +103,62 @@ export default class SpfxPromotedLinks extends React.Component<ISpfxPromotedLink
           </Pivot>
           <br/>
           <div>
-            {this.createPivotItem()}
-            {this.createPivotItem()}
-            {this.createPivotItem()}
-            {this.createPivotItem()}
-            {this.createPivotItem()}
-            {this.createPivotItem()}
-            {this.createPivotItem()}
-            {this.createPivotItem()}
+
+            {
+              /* 
+              *  for each mission passed into this component,
+              *    bind it to the ApolloMission component
+              * 
+              {this.createPivotItem()}
+              {this.createPivotItem()}
+              {this.createPivotItem()}
+              {this.createPivotItem()}
+              {this.createPivotItem()}
+              {this.createPivotItem()}
+              {this.createPivotItem()}
+              {this.createPivotItem()}
+
+                              <ApolloMission key={ this._getMissionUniqueId(mission) } 
+                              mission={ mission }
+                              onRemoveMission={ this.props.onDeleteMission } />
+
+              */
+
+              this.props.allTiles.map(newTile => (
+                <PromotedLinkItem
+                  imageUrl={newTile.imageUrl}
+                  title={newTile.title}
+                  description={newTile.description}
+                  href={newTile.href}
+                  category={newTile.category} />
+                ))
+              
+              }
+
           </div>
         </div>
       </div>
     );
   }
 
-  public onLinkClick(item: PivotItem): void {
+  private onLinkClick = (item: PivotItem): void => {
     //This sends back the correct pivot category which matches the category on the tile.
     console.log(item.props.headerText);
+    //debugger;
 
-    const filteredTiles: PromotedLinkItem[] = this.props.allTiles.filter(tile => {
+    const filteredTiles: IPromotedLinkItemProps[] = this.state.allTiles.filter(tile => tile.category === item.props.headerText);
+
+    /*
+//    const filteredTiles: PromotedLinkItem[] = this.props.allTiles.filter(tile => {   
+    var filteredTiles = this.props.allTiles.filter(tile => {
       if (
           (tile.category.toLowerCase().indexOf(item.props.headerText) === 0)
       ){
         return tile;
       }
     });
+*/
+    console.log(filteredTiles);
 
     // I think in here for the return, I need to have it just update the state and not return anything.
 
